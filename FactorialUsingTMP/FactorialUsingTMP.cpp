d8633f9603c126fe7f268138ff6ab3aa45f24ba5
Archana Shinde <144507091+archanashinde87@users.noreply.github.com>
2023-09-19 09:46:36 +0530

    Add files via upload

---
+++ FactorialUsingTMP/FactorialUsingTMP.cpp
@@ -0,0 +1,34 @@
+// FactorialUsingTMP.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+
+using namespace std;
+
+int factorial(const int input, const int sum = 1)
+{
+	if(input > 1)
+		return factorial(input - 1, sum);
+	return sum;
+}
+
+template <int input, int sum=1>
+struct Factorial:Factorial<input-1, input * sum>
+{};
+
+template<int sum>
+struct Factorial<1, sum>
+{
+	enum
+	{
+		value = sum
+	};
+};
+int main()
+{
+	cout << "5! = " << factorial(5) << endl;
+	cout << "Factorial<4> " << Factorial<4>::value << endl;
+	cout << "Factorial<5> " << Factorial<5>::value << endl;
+
+	return 0;
+}
